74932a97e2f1d838614916b7bd2774b5a20b9675
suraj <surajkiranpanda98@gmail.com>
2025-07-03 19:42:47 +0530

    commited

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
                 <div className="md:max-w-96">
                     <img className="w-36 h-auto" src={assets.logo} alt="logo" />
                     <p className="mt-6 text-sm">
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        QuickShow makes movie ticket booking easy, fast, and hassle-free. Discover the latest releases, check showtimes, and reserve your seats—all in just a few taps. Enjoy the show, we'll handle the rest!
                     </p>
                     <div className="flex items-center gap-2 mt-4">
                         <img src={assets.googlePlay} alt="google play" className="h-9 w-auto" />
@@ -28,14 +28,14 @@ const Footer = () => {
                     <div>
                         <h2 className="font-semibold mb-5">Get in touch</h2>
                         <div className="text-sm space-y-2">
-                            <p>+1-234-567-890</p>
-                            <p>contact@example.com</p>
+                            <p>+91-9861669622</p>
+                            <p>quickshow@gmail.com</p>
                         </div>
                     </div>
                 </div>
             </div>
             <p className="pt-4 text-center text-sm pb-5">
-                Copyright {new Date().getFullYear()} © GreatStack. All Right Reserved.
+                Copyright {new Date().getFullYear()} © Suraj. All Right Reserved.
             </p>
         </footer>
   )
